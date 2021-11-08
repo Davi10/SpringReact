2aecb85e81aa91fa05d361b1c2c3c1718575992d
Davi Santiago <santiago.davi10.gmail.com>
2021-11-07 23:01:49 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,11 +1,14 @@
 import axios from "axios";
+import Pagination from "components/pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { isTemplateExpression } from "typescript";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
 
-function DataTable() {
+const DataTable = () => {
+
+  const [activePage, setActivePage] = useState(0);
   const [page, setPage] = useState<SalePage>({
     first: true,
     last: true,
@@ -16,13 +19,19 @@ function DataTable() {
 
   useEffect(() => {
     axios
-      .get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`)
+      .get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
       .then((response) => {
         setPage(response.data);
       });
-  });
+  },[activePage]);
+
+  const changePage = (index: number) =>{
+    setActivePage(index);
+  }
 
   return (
+    <>
+    <Pagination page = {page} onPageChance={changePage}/>
     <div className="table-responsive">
       <table className="table table-striped table-sm">
         <thead>
@@ -47,6 +56,7 @@ function DataTable() {
         </tbody>
       </table>
     </div>
+    </>
   );
 }
 
